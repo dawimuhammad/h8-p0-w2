a2feceb3ea2a9b55ae5c2d3a9474fdb0876bf909
dawimuhammad <muhamadhaddawi@gmail.com>
2018-04-25 17:03:20 +0700

    add exercise 3 - if else

---
+++ exercise-3.js
@@ -0,0 +1,22 @@
+// phase 0 week 2 exercise 3 case if else
+
+// input
+var nama = 'Haddawi';
+var peran = 'ksatria';
+
+if (nama !== '') {
+    if (peran !== '') {
+        console.log('Selamat datang di dunia Proxytia, '+nama);
+        if (peran === 'ksatria') {
+            console.log('Halo '+peran+' '+nama+', kamu dapat menyerang dengan pedangmu!');
+        } else if (peran === 'penyihir') {
+            console.log('Halo '+peran+' '+nama+', ciptakan keajaiban yang dapat membuat rekan-rekanmu menang!');
+        } else if (peran === 'tabib') {
+            console.log('Halo '+peran+' '+nama+', kamu akan membantu rekanmu yang terluka.');
+        }
+    } else {
+        console.log('Halo '+nama+', pilih peranmu untuk memulai.');
+    }
+} else {
+    console.log('Nama harus diisi!');
+}
