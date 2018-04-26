5505c36d447bf44580de9379d23dfcbd96607053
dawimuhammad <muhamadhaddawi@gmail.com>
2018-04-26 16:03:24 +0700

    add exercise 4 - switch case

---
+++ exercise-4.js
@@ -0,0 +1,58 @@
+// phase 0 week 2 exercise 4 switch case
+
+// input
+var tanggal=15;
+var bulan=7;
+var tahun=2018;
+
+// func
+switch (bulan) {
+  case 1: {
+      console.log(tanggal+' Januari '+tahun);
+      break;
+  }
+  case 2: {
+      console.log(tanggal+' Februari '+tahun);
+      break;
+  }
+  case 3: {
+      console.log(tanggal+' Maret '+tahun);
+      break;
+  }
+  case 4: {
+      console.log(tanggal+' April '+tahun);
+      break;
+  }
+  case 5: {
+      console.log(tanggal+' Mei '+tahun);
+      break;
+  }
+  case 6: {
+      console.log(tanggal+' Juni '+tahun);
+      break;
+  }
+  case 7: {
+      console.log(tanggal+' Juli '+tahun);
+      break;
+  }
+  case 8: {
+      console.log(tanggal+' Agustus '+tahun);
+      break;
+  }
+  case 9: {
+      console.log(tanggal+' September '+tahun);
+      break;
+  }
+  case 10: {
+      console.log(tanggal+' Oktober '+tahun);
+      break;
+  }
+  case 11: {
+      console.log(tanggal+' November '+tahun);
+      break;
+  }
+  case 12: {
+      console.log(tanggal+' Desember '+tahun);
+      break;
+  }
+}
