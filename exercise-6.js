40802b3c90dde2459725d2ededa1c98a6b1181bd
dawimuhammad <muhamadhaddawi@gmail.com>
2018-05-01 21:31:02 +0700

    add exercise 6

---
+++ exercise-6.js
@@ -0,0 +1,92 @@
+// phase 0 week 2 exercise 6 looping
+
+// looping while
+//
+// var i = 2;
+//
+// console.log('LOOPING PERTAMA');
+// while (i<=20) {
+//     if (i%2 == 0) {
+//         console.log(i+' - I love coding');
+//     }
+//     i++;
+// }
+//
+// console.log('LOOPING KEDUA'+i);
+// while (i>1) {
+//     if (i%2 == 0) {
+//         console.log(i+' - I will become fullstack dev');
+//     }
+//     i--;
+// }
+//
+// // looping for
+//
+//
+// console.log('LOOPING PERTAMA');
+// for (i=1; i<=20; i++) {
+//     console.log(i+' - I love coding');
+// }
+//
+// console.log('LOOPING KEDUA');
+// for (i=20; i>0; i--) {
+//     console.log(i+' - I will be a fullstack dev');
+// }
+//
+//
+// // looping case ganjil - genap
+//
+// for (i=1; i<=100; i++) {
+//   console.log('counter sekarang = '+i);
+//   if (i%2 === 0) {
+//       console.log('output = GENAP');
+//   } else {
+//       console.log('output = GANJIL');
+//   }
+// }
+//
+
+// looping kelipatan
+
+for (i=1; i<=100; i++) {
+    if (i%3 === 0) {
+        console.log(i+' kelipatan 3');
+    } else if (i%6 === 0) {
+        console.log(i+' kelipatan 6');
+    } else if (i%10 === 0) {
+        console.log(i+' kelipatan 10');
+    } else {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end
